c6fffce39c811b85ffcae10478adb1bea22d3b0e
Wellinton03 <wellintonhf123@gmail.com>
2024-04-02 13:35:33 -0300

    Finalizada a documentação do Projeto

--- src/main/java/br/com/wellinton/gestao_vagas/candidate/CandidateEntity.java
+++ src/main/java/br/com/wellinton/gestao_vagas/candidate/CandidateEntity.java
@@ -25,16 +25,16 @@ public class CandidateEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Schema(example = "Daniel de Souza", requiredMode = RequiredMode.REQUIRED, description = "Nome do candidato")
+    @Schema(example = "José de alencar", requiredMode = RequiredMode.REQUIRED, description = "Nome do candidato")
     private String name;
 
     @NotBlank()
     @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
-    @Schema(example = "daniel", requiredMode = RequiredMode.REQUIRED, description = "Username do candidato")
+    @Schema(example = "José", requiredMode = RequiredMode.REQUIRED, description = "Username do candidato")
     private String username;
 
     @Email(message = "O campo [email] deve conter um e-mail válido")
-    @Schema(example = "daniel@gmail.com", requiredMode = RequiredMode.REQUIRED, description = "E-mail do candidato")
+    @Schema(example = "joseA@gmail.com", requiredMode = RequiredMode.REQUIRED, description = "E-mail do candidato")
     private String email;
 
     @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
